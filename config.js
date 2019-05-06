9e3a202d84bf40e9a0aeb08ada2537295a70cf90
WangZhen <wangzhenjjcn@gmail.com>
2019-05-07 01:58:15 +0800

    V1.0

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  version: "1.0.0",
+  note: 'MyAzure测试Note 增加到店自提支持',
+  subDomain: "wangzhen",  
+  appid: "wxdfefe35cc7453431", 
+  shareProfile: 'Myazure分享标语'  
+}
+ 
---
+++ wxapi/config.js
@@ -0,0 +1,7 @@
+const CONFIG = require('../config.js')
+module.exports = {
+  version: "1.0.0",
+  note: 'MyAzure测试Note 增加到店自提支持',
+  subDomain: CONFIG.subDomain
+}
+ 
